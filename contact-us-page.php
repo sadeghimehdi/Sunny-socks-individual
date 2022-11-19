b0fd12a9d20f52ee05796b545bacc60b9a67e765
sadeghimehdi <mehdi.sadeghi@student.nhlstenden.com>
2022-11-19 13:51:41 +0100

    First code for home page

---
+++ contact-us-page.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="author" content="John Doe">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Title</title>
+  <link rel="stylesheet" href="css/contact-us-page.css">
+
+</head>
+
+<body>
+<div class="image-fader">
+  <img src="https://dummyimage.com/300x200/f00/fff" />
+  <img src="https://dummyimage.com/300x200/0f0/fff" />
+  <img src="https://dummyimage.com/300x200/00f/fff" />
+  <img src="https://dummyimage.com/300x200/f0f/fff" />
+</div>
+
+
+
+</body>
---
+++ homepage.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta name="keywords" content="HTML, CSS, JavaScript">
+  <meta name="author" content="John Doe">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Title</title>
+  <link rel="stylesheet" href="css/home-page.css">
+
+</head>
+
+<body>
+
+<header>
+
+  <img src="img/sunny_logos_white.png" alt="">
+
+  <ul>
+    <li>
+      <a href="">Products</a>
+    </li>
+
+    <li>
+      <a href="">Return form</a>
+    </li>
+  </ul>
+
+</header>
+
+<main>
+
+  <div class="float-container">
+
+    <div class="transfer-container">
+      <img class="transfer-1" src="img/transfer-img1.svg" alt="">
+      <img class="transfer-2" src="img/transfer-img2.svg" alt="">
+      <img class="transfer-3" src="img/transfer-img3.svg" alt="">
+    </div>
+
+    <img src="img/cntr2.svg" alt="">
+    <img src="img/cntr3.svg" alt="">
+    <img src="img/cntr4.svg" alt="">
+
+  </div>
+
+</main>
+
+<footer>
+
+  <ul>
+    <h2>Contact us</h2>
+    <li>
+      E-mail: place.holder@gmail.com
+    </li>
+    <li>
+      Phone:  0123 567 89 01
+    </li>
+  </ul>
+
+  <img src="img/social.svg" alt="">
+
+  <h4>Most Colourful Socks website</h4>
+  <h3>Subscribe to our Newsletter</h3>
+  <form action="">
+    <label>
+      <input placeholder="Enter your email" class="email-input" type="email">
+      <input class="submit-input" type="submit">
+    </label>
+
+  </form>
+
+</footer>
+
+
+
+</body>
+
+</html>
